9628b2878756589bf17f44b1735fac8e1f003ed9
Samuel Magana <maganasamuel001@gmail.com>
2024-08-22 19:50:35 +0800

    Active scope

--- app/Models/Social.php
+++ app/Models/Social.php
@@ -3,11 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Builder, Model};
 
 class Social extends Model
 {
     use HasFactory;
+
+    public function scopeActive(Builder $query): avoid
+    {
+        return $query->where('active', true);
+    }
 
     protected function casts(): array
     {
