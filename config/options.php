df6de4e226339446412949d363b52765052a9f87
Samuel Magana <maganasamuel001@gmail.com>
2024-08-22 20:34:21 +0800

    Configure icon hint url

--- config/options.php
+++ config/options.php
@@ -4,4 +4,5 @@ return [
     'province' => [
         'id' => env('PROVINCE_ID', '0516'),
     ],
+    'icons_hint_url' => env('ICONS_HINT_URL', 'https://blade-ui-kit.com/blade-icons?set=3&search=youtube#search'),
 ];
