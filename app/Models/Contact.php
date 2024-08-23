f14ad5854b72eef5d1a8af870bc80bd215a1bf3d
Samuel Magana <maganasamuel001@gmail.com>
2024-08-23 18:15:58 +0800

    Use has order column and has active scope traits in contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,29 +2,21 @@
 
 namespace App\Models;
 
+use App\Traits\HasOrderColumn;
+use App\Traits\Scopes\HasActiveScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\{Builder, Model};
 
 class Contact extends Model
 {
     use HasFactory;
-
-    public function scopeActive(Builder $query): void
-    {
-        $query->where('active', true);
-    }
+    use HasOrderColumn;
+    use HasActiveScope;
 
     protected function casts(): array
     {
         return [
             'active' => 'boolean',
         ];
-    }
-
-    protected static function booted(): void
-    {
-        static::created(function (Contact $contact) {
-            $contact->update(['order' => $contact->id]);
-        });
     }
 }
