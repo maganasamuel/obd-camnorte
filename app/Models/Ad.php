d4cce4f0825f21754673a972aca913ae9c92477e
Samuel Magana <maganasamuel001@gmail.com>
2024-08-23 10:08:57 +0800

    Active scope, cast; Set value of order to id when created

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -3,9 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Builder, Model};
 
 class Ad extends Model
 {
     use HasFactory;
+
+    public function scopeActive(Builder $query): void
+    {
+        $query->where('active', true);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'active' => 'boolean',
+        ];
+    }
+
+    protected static function booted(): void
+    {
+        static::created(function (Ad $ad) {
+            $ad->update(['order' => $ad->id]);
+        });
+    }
 }
