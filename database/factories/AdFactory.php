cfe77c3ffa741eb6eacd0d30bdc2026c556ce970
Samuel Magana <maganasamuel001@gmail.com>
2024-08-23 18:04:19 +0800

    Add ineffective state to ad factory

--- database/factories/AdFactory.php
+++ database/factories/AdFactory.php
@@ -18,6 +18,10 @@ class AdFactory extends Factory
     {
         return [
             'name' => fake()->company(),
+            'effective_from' => now(),
+            'effective_to' => function (array $attributes) {
+                return $attributes['effective_from']->clone()->addDays(random_int(5, 30));
+            },
             'active' => true,
         ];
     }
@@ -27,6 +31,18 @@ class AdFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'active' => false,
+            ];
+        });
+    }
+
+    public function ineffective(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'effective_to' => now()->subDays(random_int(1, 5)),
+                'effective_from' => function (array $attributes) {
+                    return $attributes['effective_to']->clone()->subDays(random_int(5, 30));
+                },
             ];
         });
     }
