31ca8ce2904ca17830d1dbf1ff0e2491cbd765ed
Samuel Magana <maganasamuel001@gmail.com>
2024-08-23 10:08:27 +0800

    Set columns for ads migration

--- database/migrations/2024_08_23_003005_create_ads_table.php
+++ database/migrations/2024_08_23_003005_create_ads_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name')->unique();
+            $table->boolean('active')->default(1);
+            $table->unsignedTinyInteger('order')->nullable();
+
             $table->timestamps();
         });
     }
