42f8b71484bd775ef96948cc80115b57a0d760be
Samuel Magana <maganasamuel001@gmail.com>
2024-08-25 20:49:41 +0800

    Separate sections of landing page

---
+++ resources/views/landing/testimonials.blade.php
@@ -0,0 +1,34 @@
+<!-- Testimonials -->
+<div class="bg-black">
+  <div class="px-4 py-12 mx-auto max-w-7xl lg:px-6 lg:px-8 lg:py-24">
+    <blockquote class="max-w-4xl mx-auto">
+      <p class="mb-6 md:text-lg">
+        <span class="font-semibold text-lime-400">Philip,</span> <span class="text-neutral-500">CEO at Day Spa</span>
+      </p>
+
+      <p class="text-xl text-white sm:text-2xl md:text-3xl md:leading-normal">
+        I'm absolutely floored by the level of care and attention to detail the team at Preline have put into this project and for one can guarantee that we will be a return customer.
+      </p>
+
+      <footer class="mt-6 md:mt-10">
+        <div class="border-neutral-700">
+          <button type="button"
+            class="inline-flex items-center text-sm group gap-x-3 text-neutral-400 focus:outline-none disabled:opacity-50 disabled:pointer-events-none">
+            <span class="flex flex-col items-center justify-center text-black bg-white rounded-full size-8 md:size-10 group-hover:bg-lime-400 group-focus:bg-lime-400">
+              <svg class="flex-shrink-0 size-5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16">
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
+              </svg>
+            </span>
+            Watch the Video
+          </button>
+        </div>
+      </footer>
+    </blockquote>
+  </div>
+</div>
+<!-- End Testimonials -->
---
+++ resources/views/landing/works.blade.php
@@ -0,0 +1,146 @@
+<!-- Works -->
+<div class="px-4 py-12 mx-auto max-w-7xl lg:px-6 lg:px-8 lg:py-24">
+  <div class="max-w-2xl mx-auto mb-6 text-center sm:mb-10">
+    <h1 class="text-2xl font-medium text-black sm:text-4xl dark:text-white">
+      Latest work
+    </h1>
+  </div>
+
+  <!-- Card Grid -->
+  <div class="grid grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+    <!-- Card -->
+    <a class="block group"
+      href="#">
+      <div class="overflow-hidden bg-gray-100 aspect-w-16 aspect-h-12 rounded-2xl dark:bg-neutral-800">
+        <img class="object-cover transition-transform duration-500 ease-in-out group-hover:scale-105 rounded-2xl"
+          src="https://images.unsplash.com/photo-1575052814086-f385e2e2ad1b?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image Description">
+      </div>
+
+      <div class="pt-4">
+        <h3
+          class="relative inline-block font-medium text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+          eYoga
+        </h3>
+        <p class="mt-1 text-gray-600 dark:text-neutral-400">
+          A revamped and dynamic approach to yoga analytics
+        </p>
+
+        <div class="flex flex-wrap gap-2 mt-3">
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Discovery
+          </span>
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Brand Guidelines
+          </span>
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Yoga
+          </span>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="block group"
+      href="#">
+      <div class="overflow-hidden bg-gray-100 aspect-w-16 aspect-h-12 rounded-2xl dark:bg-neutral-800">
+        <img class="object-cover transition-transform duration-500 ease-in-out group-hover:scale-105 rounded-2xl"
+          src="https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image Description">
+      </div>
+
+      <div class="pt-4">
+        <h3
+          class="relative inline-block font-medium text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+          Nike React
+        </h3>
+        <p class="mt-1 text-gray-600 dark:text-neutral-400">
+          Rewriting sport's playbook for billions of athletes
+        </p>
+
+        <div class="flex flex-wrap gap-2 mt-3">
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Brand Strategy
+          </span>
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Visual Identity
+          </span>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="block group"
+      href="#">
+      <div class="overflow-hidden bg-gray-100 aspect-w-16 aspect-h-12 rounded-2xl dark:bg-neutral-800">
+        <img class="object-cover transition-transform duration-500 ease-in-out group-hover:scale-105 rounded-2xl"
+          src="https://images.unsplash.com/photo-1649999920973-ab6bfd0c0017?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image Description">
+      </div>
+
+      <div class="pt-4">
+        <h3
+          class="relative inline-block font-medium text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+          Day Spa
+        </h3>
+        <p class="mt-1 text-gray-600 dark:text-neutral-400">
+          Designing a new cocktail can
+        </p>
+
+        <div class="flex flex-wrap gap-2 mt-3">
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Brand Strategy
+          </span>
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Visual Identity
+          </span>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="block group"
+      href="#">
+      <div class="overflow-hidden bg-gray-100 aspect-w-16 aspect-h-12 rounded-2xl dark:bg-neutral-800">
+        <img class="object-cover transition-transform duration-500 ease-in-out group-hover:scale-105 rounded-2xl"
+          src="https://images.unsplash.com/photo-1528291954423-c0c71c12baeb?q=80&w=3426&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image Description">
+      </div>
+
+      <div class="pt-4">
+        <h3
+          class="relative inline-block font-medium text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 before:transition before:origin-left before:scale-x-0 group-hover:before:scale-x-100 dark:text-white">
+          Diamond Dynamics
+        </h3>
+        <p class="mt-1 text-gray-600 dark:text-neutral-400">
+          From cutting-edge equipment to stylish apparel
+        </p>
+
+        <div class="flex flex-wrap gap-2 mt-3">
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Sports Gear
+          </span>
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Equipment
+          </span>
+          <span class="py-1.5 px-3 bg-white text-gray-600 border border-gray-200 text-xs sm:text-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+            Discovery
+          </span>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+  </div>
+  <!-- End Card Grid -->
+
+  <div class="mt-10 text-center lg:mt-20">
+    <a class="relative inline-block font-medium md:text-lg text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 hover:before:bg-black focus:outline-none focus:before:bg-black dark:text-white dark:hover:before:bg-white dark:focus:before:bg-white"
+      href="#">
+      View all Work
+    </a>
+  </div>
+</div>
+<!-- End Works -->
