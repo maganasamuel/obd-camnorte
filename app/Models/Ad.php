683ca06bdcd2cd1ed17a792923cb39fa727720ae
Samuel Magana <maganasamuel001@gmail.com>
2024-08-25 12:55:30 +0800

    Register media conversions

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -7,6 +7,7 @@ use App\Traits\HasOrderColumn;
 use App\Traits\Scopes\{HasActiveScope, HasEffectiveScope};
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\{Builder, Model};
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\MediaLibrary\{HasMedia, InteractsWithMedia};
 
 class Ad extends Model implements HasMedia
@@ -22,6 +23,14 @@ class Ad extends Model implements HasMedia
         'effectivity',
         'effective_period',
     ];
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(192)
+            ->height(108)
+            ->nonQueued();
+    }
 
     protected function casts(): array
     {
