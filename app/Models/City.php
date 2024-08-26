5bf960c30c732cb2e1fdf854e8d75d388b8d90e0
Samuel Magana <maganasamuel001@gmail.com>
2024-08-26 10:33:22 +0800

    Provinced scope for city model

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
 use Yajra\Address\Entities\City as YajraCity;
 
@@ -15,5 +16,10 @@ class City extends YajraCity
     public function barangays(): HasMany
     {
         return $this->hasMany(Barangay::class, 'city_id', 'city_id');
+    }
+
+    public function scopeProvinced(Builder $query): void
+    {
+        $query->where('province_id', config('options.province.id'));
     }
 }
