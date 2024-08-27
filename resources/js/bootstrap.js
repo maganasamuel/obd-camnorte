98042540fd26da0f3fadae769d532448e64875f1
Samuel Magana <maganasamuel001@gmail.com>
2024-08-27 20:22:32 +0800

    Installed gumshoe

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,3 +4,6 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import 'preline';
+
+import Gumshoe from 'gumshoejs';
+window.Gumshoe = Gumshoe;
