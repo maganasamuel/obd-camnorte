c743a850903b4caf3647bb8900c0ebcae5bee9c4
Samuel Magana <maganasamuel001@gmail.com>
2024-08-27 22:16:54 +0800

    Infinite scroll animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ export default {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
     },
   },
 
