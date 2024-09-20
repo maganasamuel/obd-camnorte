65d1208b762e4ecd051b1b7414115b8ab2c32245
maganasamuel <maganasamuel001@gmail.com>
2024-09-20 14:54:51 +0800

    Set web ads animation

--- resources/views/landing/ads-web.blade.php
+++ resources/views/landing/ads-web.blade.php
@@ -1,4 +1,24 @@
-<div @class(['relative hidden shrink-0', 'lg:block' => $ads->count() >= 2])>
+<div @class(['relative hidden shrink-0', 'lg:block' => $ads->count() >= 2])
+  x-data="{ logosHeight: 0, logosWrapperHeight: 0, 'position': '{{ ['left' => 'afterend', 'right' => 'beforebegin'][$position] }}' }"
+  x-init="$nextTick(() => {
+      logosHeight = $refs.logos.clientHeight;
+  
+      logosWrapperHeight = $refs.logosWrapper.clientHeight;
+  
+      if (logosHeight < logosWrapperHeight) {
+          return;
+      }
+  
+      let ul = $refs.logos;
+  
+      ul.insertAdjacentHTML(position, ul.outerHTML);
+  
+      try {
+          ul.nextSibling.setAttribute('aria-hidden', 'true');
+      } catch (error) {}
+  
+  });"
+  x-ref="logosWrapper">
   <div @class([
       'grid gap-4',
       'grid-cols-1' => $adsWeb[$position]->count() <= 1,
@@ -15,17 +35,18 @@
       'left-0' => $position == 'left',
       'right-0' => $position == 'right',
   ])>
-    <div x-data
-      x-ref="logosWrapper">
+    <div>
       <div @class([
           'relative overflow-hidden before:absolute before:top-0 before:start-0 before:z-10 before:h-10 before:w-full before:bg-gradient-to-b before:from-white before:to-transparent after:absolute after:top-0 after:end-0 after:h-10 after:w-full after:bg-gradient-to-t after:from-white after:to-transparent dark:before:from-neutral-900 dark:after:from-neutral-900',
       ])>
-        <div>
+        <div class="space-y-4">
           <ul @class([
               'grid gap-4',
               'grid-cols-1' => $adsWeb[$position]->count() <= 1,
               'grid-cols-2' => $adsWeb[$position]->count() >= 2,
           ])
+            x-bind:class="{ '{{ ['left' => 'animate-loop-scroll-up', 'right' => 'animate-loop-scroll-down'][$position] }}': logosHeight >= logosWrapperHeight }"
+            style="animation-duration: {{ $adsWeb[$position]->count() * 3 }}s;"
             x-ref="logos">
             @foreach ($adsWeb[$position] as $ad)
               <li>
