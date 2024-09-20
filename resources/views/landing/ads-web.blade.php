f65b5b9949e65c0340632fdad36fd719fa0e95e5
maganasamuel <maganasamuel001@gmail.com>
2024-09-20 15:24:25 +0800

    Update web ads gradient

--- resources/views/landing/ads-web.blade.php
+++ resources/views/landing/ads-web.blade.php
@@ -1,4 +1,9 @@
-<div @class(['relative hidden shrink-0', 'lg:block' => $ads->count() >= 2])
+<div @class([
+    'relative hidden shrink-0',
+    'lg:block' => $ads->count() >= 2,
+    'before:absolute before:top-0 before:start-0 before:z-10 before:h-11 before:w-full before:bg-gradient-to-b before:from-white before:to-transparent after:absolute after:bottom-0 after:start-0 after:h-11 after:w-full after:bg-gradient-to-t after:from-white after:to-transparent dark:before:from-neutral-900 dark:after:from-neutral-900',
+    '[mask-image:_linear-gradient(to_bottom,transparent_0,_black_96px,_black_calc(100%-96px),transparent_100%)]',
+])
   x-data="{ logosHeight: 0, logosWrapperHeight: 0, 'position': '{{ ['left' => 'afterend', 'right' => 'beforebegin'][$position] }}' }"
   x-init="$nextTick(() => {
       logosHeight = $refs.logos.clientHeight;
@@ -35,29 +40,23 @@
       'left-0' => $position == 'left',
       'right-0' => $position == 'right',
   ])>
-    <div>
-      <div @class([
-          'relative overflow-hidden before:absolute before:top-0 before:start-0 before:z-10 before:h-10 before:w-full before:bg-gradient-to-b before:from-white before:to-transparent after:absolute after:top-0 after:end-0 after:h-10 after:w-full after:bg-gradient-to-t after:from-white after:to-transparent dark:before:from-neutral-900 dark:after:from-neutral-900',
-      ])>
-        <div class="space-y-4">
-          <ul @class([
-              'grid gap-4',
-              'grid-cols-1' => $adsWeb[$position]->count() <= 1,
-              'grid-cols-2' => $adsWeb[$position]->count() >= 2,
-          ])
-            x-bind:class="{ '{{ ['left' => 'animate-loop-scroll-up', 'right' => 'animate-loop-scroll-down'][$position] }}': logosHeight >= logosWrapperHeight }"
-            style="animation-duration: {{ $adsWeb[$position]->count() * 3 }}s;"
-            x-ref="logos">
-            @foreach ($adsWeb[$position] as $ad)
-              <li>
-                <img src="{{ $ad->getFirstMediaUrl('ads', 'thumb') }}"
-                  alt="{{ $ad->name }}"
-                  class="object-contain object-center h-[90px] w-[160px] rounded-lg border border-gray-200 dark:border-neutral-700 shadow p-1" />
-              </li>
-            @endforeach
-          </ul>
-        </div>
-      </div>
+    <div class="space-y-4">
+      <ul @class([
+          'grid gap-4',
+          'grid-cols-1' => $adsWeb[$position]->count() <= 1,
+          'grid-cols-2' => $adsWeb[$position]->count() >= 2,
+      ])
+        x-bind:class="{ '{{ ['left' => 'animate-loop-scroll-up', 'right' => 'animate-loop-scroll-down'][$position] }}': logosHeight >= logosWrapperHeight }"
+        style="animation-duration: {{ $adsWeb[$position]->count() * 3 }}s;"
+        x-ref="logos">
+        @foreach ($adsWeb[$position] as $ad)
+          <li>
+            <img src="{{ $ad->getFirstMediaUrl('ads', 'thumb') }}"
+              alt="{{ $ad->name }}"
+              class="object-contain object-center h-[90px] w-[160px] rounded-lg border border-gray-200 dark:border-neutral-700 shadow p-1" />
+          </li>
+        @endforeach
+      </ul>
     </div>
   </div>
 </div>
